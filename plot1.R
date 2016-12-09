1a9a0e74ca1b46eb09f687f9f4fcda9b557e0138
mlahozruiz <mlahozruiz@gmail.com>
2016-12-09 18:22:29 +0100

    plot 1

---
+++ plot1.R
@@ -0,0 +1,6 @@
+#load dataset
+datos_completos<- read.table("household_power_consumption.txt",header = TRUE, sep = ";", na.strings="?")
+# select the interval we want
+datos <- datos_completos[(datos_completos$Date=="1/2/2007" | datos_completos$Date=="2/2/2007" ), ]
+# draw the histogram
+hist(datos$Global_active_power, col = "red", xlab = "Global Active power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
