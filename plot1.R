ed297dec5b4bdbe2fc09dea39a535972ac97081a
mlahozruiz <mlahozruiz@gmail.com>
2016-12-09 18:23:56 +0100

    update create file

--- plot1.R
+++ plot1.R
@@ -4,3 +4,6 @@ datos_completos<- read.table("household_power_consumption.txt",header = TRUE, se
 datos <- datos_completos[(datos_completos$Date=="1/2/2007" | datos_completos$Date=="2/2/2007" ), ]
 # draw the histogram
 hist(datos$Global_active_power, col = "red", xlab = "Global Active power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+#create the png file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
